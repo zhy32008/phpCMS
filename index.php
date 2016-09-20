48ea301c030b7d8ede15c467382178808dd71df2
dennis <zhaohongyong@koubeilvxing.com>
2016-09-20 18:43:23 +0800

    init

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dennis
+ * Date: 16/9/19
+ * Time: 下午9:14
+ */
+
+define('APP_PATH','./Application/');
+define('APP_NAME','CMS');
+define('APP_DEBUG',true);
+
+require ('./ThinkPHP/ThinkPHP.php');
